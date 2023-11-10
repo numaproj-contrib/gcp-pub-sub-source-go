5db643a801f9c4a5529213675365fcb9fc9fc0a3
shubham dixit <shubhamdixit863@gmail.com>
2023-11-10 17:13:16 +0530

    integration tests

--- pkg/pubsubsource/pubsub_test.go
+++ pkg/pubsubsource/pubsub_test.go
@@ -3,15 +3,16 @@ package pubsubsource
 import (
 	"context"
 	"fmt"
-	"github.com/numaproj-contrib/gcp-pub-sub-source-go/pkg/mocks"
-	"github.com/numaproj/numaflow-go/pkg/sourcer"
-	"github.com/stretchr/testify/assert"
 	"log"
 	"os"
 	"testing"
 	"time"
 
 	"cloud.google.com/go/pubsub"
+	"github.com/numaproj/numaflow-go/pkg/sourcer"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/numaproj-contrib/gcp-pub-sub-source-go/pkg/mocks"
 )
 
 const TopicID = "pubsub-test"
@@ -42,34 +43,46 @@ func sendMessages(ctx context.Context, client *pubsub.Client, topicID string) {
 	}
 }
 
-func ensureTopicAndSubscription(ctx context.Context, client *pubsub.Client, topicID, subID string) {
+func ensureTopicAndSubscription(ctx context.Context, client *pubsub.Client, topicID, subID string) error {
 	// Ensure topic exists.
 	topic := client.Topic(topicID)
 	exists, err := topic.Exists(ctx)
 	if err != nil {
-		log.Fatalf("Failed to check if topic exists: %v", err)
+		return err
+
 	}
 	if !exists {
+
 		if _, err = client.CreateTopic(ctx, topicID); err != nil {
-			log.Fatalf("Failed to create the topic: %v", err)
+			return err
 		}
+
 	}
 
 	// Ensure subscription exists.
 	sub := client.Subscription(subID)
 	exists, err = sub.Exists(ctx)
 	if err != nil {
-		log.Fatalf("Failed to check if the subscription exists: %v", err)
+		return err
+
 	}
 	if !exists {
+		log.Println("subscription doesnt exists")
 		if _, err = client.CreateSubscription(ctx, subID, pubsub.SubscriptionConfig{Topic: topic}); err != nil {
-			log.Fatalf("Failed to create the subscription: %v", err)
+			return err
+
 		}
 	}
+	return nil
 }
 
 func TestMain(m *testing.M) {
-	os.Setenv("PUBSUB_EMULATOR_HOST", "localhost:8085")
+	var err error
+
+	err = os.Setenv("PUBSUB_EMULATOR_HOST", "localhost:8085")
+	if err != nil {
+		log.Fatalf("error -%s", err)
+	}
 
 	ctx := context.Background()
 
@@ -77,14 +90,11 @@ func TestMain(m *testing.M) {
 	projectID := "just-ratio-366415"
 
 	// Creates a client.
-	var err error
 	pubsubClient, err = pubsub.NewClient(ctx, projectID)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 	defer pubsubClient.Close()
-
-	ensureTopicAndSubscription(ctx, pubsubClient, TopicID, subscriptionID)
 
 	code := m.Run()
 	os.Exit(code)
@@ -94,77 +104,94 @@ func TestMain(m *testing.M) {
 
 func TestPubSubSource_Read(t *testing.T) {
 
+	err := ensureTopicAndSubscription(context.TODO(), pubsubClient, TopicID, subscriptionID)
+	assert.Nil(t, err)
 	messageCh := make(chan sourcer.Message, 20)
 	//doneCh := make(chan struct{})
-
-	sub := pubsubClient.Subscription(TopicID)
-	pubsubsource := NewPubSubSource(pubsubClient, sub)
-
-	sendMEssagech := make(chan struct{})
+	exitch := make(chan struct{})
+	sendMEssagech := make(chan int)
 	go func() {
-		sendMessages(context.Background(), pubsubClient, TopicID)
-		close(sendMEssagech)
-	}()
-	<-sendMEssagech
+		for i := 0; i < 20; i++ {
+			if err := publish(context.Background(), pubsubClient, TopicID, fmt.Sprintf("Message %d", i)); err != nil {
+				log.Fatalf("Failed to publish: %v", err)
+			}
+			if i%5 == 0 {
+				sendMEssagech <- i
 
-	pubsubsource.Read(context.TODO(), mocks.ReadRequest{
-		CountValue: 5,
-		Timeout:    10 * time.Second,
-	}, messageCh)
+			}
+		}
+		close(exitch)
+	}()
+
+	for {
+		select {
+		case <-sendMEssagech:
+			sub := pubsubClient.Subscription(subscriptionID)
+			pubsubsource := NewPubSubSource(pubsubClient, sub)
+			log.Println("Received called")
+			pubsubsource.Read(context.TODO(), mocks.ReadRequest{
+				CountValue: 5,
+				Timeout:    10 * time.Second,
+			}, messageCh)
+			assert.Equal(t, 5, len(messageCh))
+		case <-exitch:
+			break
+
+		}
+	}
 
 	//<-doneCh
 
-	assert.Equal(t, 5, len(messageCh))
-	// Try reading 4 more messages
-	// Since the previous batch didn't get acked, the data source shouldn't allow us to read more messages
-	// We should get 0 messages, meaning the channel only holds the previous 5 messages
-	pubsubsource.Read(context.TODO(), mocks.ReadRequest{
-		CountValue: 4,
-		Timeout:    5 * time.Second,
-	}, messageCh)
+	/*
+		// Try reading 4 more messages
+		// Since the previous batch didn't get acked, the data source shouldn't allow us to read more messages
+		// We should get 0 messages, meaning the channel only holds the previous 5 messages
+		pubsubsource.Read(context.TODO(), mocks.ReadRequest{
+			CountValue: 4,
+			Timeout:    5 * time.Second,
+		}, messageCh)
 
-	assert.Equal(t, 5, len(messageCh))
-
+		assert.Equal(t, 5, len(messageCh))
+	*/
 	// Ack the first batch
-	msg1 := <-messageCh
-	msg2 := <-messageCh
-	msg3 := <-messageCh
-	msg4 := <-messageCh
-	msg5 := <-messageCh
-
-	pubsubsource.Ack(context.TODO(), mocks.TestAckRequest{
-		OffsetsValue: []sourcer.Offset{msg1.Offset(), msg2.Offset(), msg3.Offset(), msg4.Offset(), msg5.Offset()},
-	})
-
-	sub2 := pubsubClient.Subscription(TopicID)
-
-	sendMEssagech2 := make(chan struct{})
-	go func() {
-		sendMessages(context.Background(), pubsubClient, TopicID)
-		close(sendMEssagech2)
-	}()
-	<-sendMEssagech2
-
-	pubsubsource2 := NewPubSubSource(pubsubClient, sub2)
-
-	pubsubsource2.Read(context.TODO(), mocks.ReadRequest{
-		CountValue: 4,
-		Timeout:    30 * time.Second,
-	}, messageCh)
-
-	assert.Equal(t, 4, len(messageCh))
 
 	/*
 
 
+		msg1 := <-messageCh
+			msg2 := <-messageCh
+			msg3 := <-messageCh
+			msg4 := <-messageCh
+			msg5 := <-messageCh
+
+			pubsubsource.Ack(context.TODO(), mocks.TestAckRequest{
+				OffsetsValue: []sourcer.Offset{msg1.Offset(), msg2.Offset(), msg3.Offset(), msg4.Offset(), msg5.Offset()},
+			})
+
+			sub2 := pubsubClient.Subscription(subscriptionID)
+
+			sendMEssagech2 := make(chan struct{})
+			go func() {
+				sendMessages(context.Background(), pubsubClient, TopicID)
+				close(sendMEssagech2)
+			}()
+			<-sendMEssagech2
+
+			pubsubsource2 := NewPubSubSource(pubsubClient, sub2)
+
+			pubsubsource2.Read(context.TODO(), mocks.ReadRequest{
+				CountValue: 4,
+				Timeout:    30 * time.Second,
+			}, messageCh)
+
+			assert.Equal(t, 4, len(messageCh))
 
 
 
+			t.Log("Calling ReAD AGAIN---------")
 
-		t.Log("Calling ReAD AGAIN---------")
-
-		//time.Sleep(10 * time.Second)
-		sendMEssage(topic)
+			//time.Sleep(10 * time.Second)
+			sendMEssage(topic)
 
 
 
@@ -172,5 +199,6 @@ func TestPubSubSource_Read(t *testing.T) {
 
 	//err := sub.Delete(context.Background())
 	//assert.Nil(t, err)
-	log.Println("Subscription Deleted--------")
+	//log.Println("Subscription Deleted--------")
+
 }
